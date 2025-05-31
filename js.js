fdda849f237713e90c50e4edd22b6f4847eddd0a
Caneesha <sophie.neufahrt@t-online.de>
2025-05-31 12:00:54 +0200

    Add loops for grid-divs and style it

--- js.js
+++ js.js
@@ -1,5 +1,14 @@
 const container = document.querySelector("#container");
 
-let gridDiv = document.createElement("div");
-gridDiv.classList.add("gridDiv");
-container.appendChild(gridDiv);
+
+for (i = 0; i < 4; i++){
+    let smallContainer = document.createElement("div");
+    smallContainer.classList.add("smallContainer");
+    container.appendChild(smallContainer);
+
+    for (j = 0; j < 4; j++){
+        let gridDiv = document.createElement("div");
+        gridDiv.classList.add("gridDiv");
+        smallContainer.appendChild(gridDiv);
+    }
+}
